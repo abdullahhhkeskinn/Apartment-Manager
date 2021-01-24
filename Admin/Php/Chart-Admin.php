e8722afa17e8967425947f228661e18f5358c66f
Abdullah Keskin <abdullahhhkeskinn@gmail.com>
2021-01-24 16:50:59 +0300

    php and sql codes done for monthly revenue chart

--- Admin/Php/Chart-Admin.php
+++ Admin/Php/Chart-Admin.php
@@ -1,18 +1,56 @@
 <?php
 
+$paid_dues = 0;
+$paid_expenses = 0;
+$all_expenses = 0;
+
+$paid_dues_sql = "SELECT fee FROM due_user_flat INNER JOIN dues USING (dueId) WHERE is_paid = 1 AND MONTH(pay_date) = MONTH(CURRENT_DATE()) AND YEAR(pay_date) = YEAR(CURRENT_DATE()) ";
+$paid_expenses_sql = "SELECT fee FROM expense_user_flat INNER JOIN expenses USING (expenseId) WHERE is_paid = 1 AND MONTH(pay_date) = MONTH(CURRENT_DATE()) AND YEAR(pay_date) = YEAR(CURRENT_DATE()) ";
+$all_expense_fee_sql = "SELECT * FROM expense_user_flat  INNER JOIN expenses USING (expenseId) WHERE MONTH(expenseDate) = MONTH(CURRENT_DATE()) AND YEAR(expenseDate) = YEAR(CURRENT_DATE())";
+
+$sql_return = mysqli_query($conn,$paid_dues_sql);
+if (mysqli_num_rows($sql_return) > 0) {
+	while ($result = mysqli_fetch_array($sql_return)) {
+		$paid_dues += $result['fee'];
+	}
+}
+
+$sql_return = mysqli_query($conn,$paid_expenses_sql);
+if (mysqli_num_rows($sql_return) > 0) {
+	while ($result = mysqli_fetch_array($sql_return)) {
+		$paid_expenses += $result['fee'];
+	}
+}
+
+$sql_return = mysqli_query($conn,$all_expense_fee_sql);
+if (mysqli_num_rows($sql_return) > 0) {
+	while ($result = mysqli_fetch_array($sql_return)) {
+		$all_expenses += $result['fee'];
+	}
+}
+
 $dataPoints = array(
-	array("label" => "Paid Expenses",  "y" => 1500),
-	array("label" => "Paid Dues", "y" => 500),
+	array("label" => "Paid Expenses",  "y" => $paid_expenses),
+	array("label" => "Paid Dues", "y" => $paid_dues ),
 	array("label" => "Total Revenue", "isIntermediateSum" => true),
-	array("label" => "Expenses", "y" => -1300),
+	array("label" => "Expenses", "y" => -$all_expenses),
 	array("label" => "Net Income",  "isCumulativeSum" => true)
 );
 
+?>
+<?php 
+	
+	$sname = "localhost";
+	$uname = "root";
+	$password = "";
+	$db_name = "apartmentmanager";
+	$conn  = mysqli_connect($sname, $uname, $password, $db_name);
 ?>
 <!DOCTYPE HTML>
 <html>
 
 <head>
+
 	<script>
 		window.onload = function() {
 
