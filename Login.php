406532e2341a5605a916bca74f94ae5b6501f9b1
Abdullah Keskin <abdullahhhkeskinn@gmail.com>
2021-01-28 21:33:34 +0300

    login page fixes for moved out residents

--- Login.php
+++ Login.php
@@ -9,50 +9,61 @@
 </head>
 
 <body>
-    
-<?php
+
+  <?php
   require("Php-Connection.php");
-?>
+  ?>
 
   <div class="main">
     <p class="sign" align="center">Sign in</p>
-    <form class="form1" method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
+    <form class="form1" method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
       <input class="un " type="text" align="center" name="Uname" placeholder="Username">
       <input class="pass" type="password" align="center" name="password" placeholder="Password">
-      <input type="submit" class="submit" align="center"></a>     
-    </div>   
+      <input type="submit" class="submit" align="center"></a>
+  </div>
 
-    <?php
-        if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $userName = $_POST['Uname'];
-        $password = $_POST['password'] ;
-        $md5_password = md5($password);
-        $checkInfoSql = "SELECT * FROM users WHERE username = '$userName' AND pass = '$md5_password' ";
-        
-        $result = mysqli_query($conn,$checkInfoSql);
-        if(isset($userName) && isset($password)){
-            if (mysqli_num_rows($result)== 1) {
-                $results = mysqli_fetch_array($result);
-                $userId = $results['userId'];
-                $flatNo = $results['flatNo'];
-                $_SESSION["userId"] = $userId;
-                $_SESSION["flatNo"] = $flatNo;
-                if($results['isAdmin']== 1){
-                  
-                  header("Location: Admin/Home-Admin.php");
-                  exit();
-                }
-                else{
-                  header("Location: User/Home-User.php");
-                  exit();
-                } 
-            }
-            else{
-                echo "<script type='text/javascript'>alert('Wrong password or username');</script>";
-            }            
-        }            
+  <?php
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $userName = $_POST['Uname'];
+    $password = $_POST['password'];
+    $md5_password = md5($password);
+    $checkInfoSql = "SELECT * FROM users WHERE username = '$userName' AND pass = '$md5_password' ";
+
+    $result = mysqli_query($conn, $checkInfoSql);
+    if (isset($userName) && isset($password)) {
+      if (mysqli_num_rows($result) == 1) {
+        $isMoved = $results['isMoved'];
+        if ($isMoved == 0) {
+          $results = mysqli_fetch_array($result);
+          $userId = $results['userId'];
+          $flatNo = $results['flatNo'];
+          $_SESSION["userId"] = $userId;
+          $_SESSION["flatNo"] = $flatNo;
+          if ($results['isAdmin'] == 1) {
+
+            header("Location: Admin/Home-Admin.php");
+            exit();
+          } else {
+            header("Location: User/Home-User.php");
+            exit();
+          }
+        } else {
+          echo "<script type='text/javascript'>alert('This user is no longer resides in this Apartment');</script>";
+        }
+      } else {
+        echo "<script type='text/javascript'>alert('Wrong password or username');</script>";
+      }
     }
-    ?>
+  }
+  ?>
 
 </body>
+<footer>
+  <script>
+    if (window.history.replaceState) {
+      window.history.replaceState(null, null, window.location.href);
+    }
+  </script>
+</footer>
+
 </html>
