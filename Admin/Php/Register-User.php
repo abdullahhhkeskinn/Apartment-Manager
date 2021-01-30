72c3ee0e49c149de0908b332c217ae5541806c2b
Abdullah Keskin <abdullahhhkeskinn@gmail.com>
2021-01-30 21:15:00 +0300

    Small control fixes

--- Admin/Php/Register-User.php
+++ Admin/Php/Register-User.php
@@ -1,37 +1,42 @@
 <?php
-require("../../Php-Connection.php");
-if(isset($_POST['userSubmit'])){
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = $_POST['name'];
-    $surname = $_POST['surname'];
-    $dob = $_POST['dob'];
-    $username = $_POST['username'];
-    $pass = $_POST['pass'];
-    $md5_password = md5($pass);
-    $email = $_POST['email'];
-    $phoneNumber = $_POST['phoneNum'];
-    $apartmentNo = $_POST['flatNo'];
-    $admin = $_POST['admin'];
+    require("../../Php-Connection.php");
+    if(isset($_POST['userSubmit'])){
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $name = $_POST['name'];
+            $surname = $_POST['surname'];
+            $dob = $_POST['dob'];
+            $username = $_POST['username'];
+            $pass = $_POST['pass'];
+            $md5_password = md5($pass);
+            $email = $_POST['email'];
+            $phoneNumber = $_POST['phoneNum'];
+            $apartmentNo = $_POST['flatNo'];
+            $admin = $_POST['admin'];
 
-    $check = "SELECT * FROM users WHERE username = '$username' ";
-    $result = mysqli_query($conn, $check);
-    if (mysqli_num_rows($result) == 1) {
-        echo "<script type='text/javascript'>alert('This username is already taken');</script>";
-        echo("<script>window.location ='../Register-Admin.php';</script>");
-    } else {
-        $sql =  "INSERT INTO users(`fname`, `surname`, `dob`, `username`, `pass`, `mail`, `phoneNo`, `isAdmin`) VALUES ('$name','$surname','$dob',
-        '$username','$md5_password','$email','$phoneNumber','$admin')";
+            $check = "SELECT * FROM users WHERE username = '$username' ";
+            $check2 = "SELECT * FROM user_flat WHERE apartmentNum = '$apartmentNo' ";
+            $result = mysqli_query($conn, $check);
+            $result2 = mysqli_query($conn, $check2);
+            if (mysqli_num_rows($result) == 1) {
+                echo "<script type='text/javascript'>alert('This username is already taken');</script>";
+                echo("<script>window.location ='../Register-Admin.php';</script>");
+            } else {
+                if(mysqli_num_rows($result2) == 1){
+                    echo "<script type='text/javascript'>alert('This Flat Has Already A User');</script>";
+                    echo("<script>window.location ='../Register-Admin.php';</script>");
+                }else{
+                $sql =  "INSERT INTO users(`fname`, `surname`, `dob`, `username`, `pass`, `mail`, `phoneNo`, `isAdmin`) VALUES ('$name','$surname','$dob',
+                    '$username','$md5_password','$email','$phoneNumber','$admin')";
 
-        if (mysqli_query($conn, $sql)) {
-            $userIdArr = mysqli_fetch_array(mysqli_query($conn, "SELECT * FROM users WHERE userId=(SELECT max(userId) FROM users)"));
-            $userId = $userIdArr['userId'];
-            mysqli_query($conn,"INSERT INTO user_flat(`userId`, `apartmentNum`) VALUES ('$userId','$apartmentNo')");
-            echo "<script type='text/javascript'>alert('User Succesfully Created');</script>";
-            echo("<script>window.location ='../Register-Admin.php';</script>");
-            
+                    if (mysqli_query($conn, $sql)) {
+                        $userIdArr = mysqli_fetch_array(mysqli_query($conn, "SELECT * FROM users WHERE userId=(SELECT max(userId) FROM users)"));
+                        $userId = $userIdArr['userId'];
+                        mysqli_query($conn,"INSERT INTO user_flat(`userId`, `apartmentNum`) VALUES ('$userId','$apartmentNo')");
+                        echo "<script type='text/javascript'>alert('User Succesfully Created');</script>";
+                        echo("<script>window.location ='../Register-Admin.php';</script>");
+                    } 
+                }
             }
         }
     }
-}
-
 ?>
--- Admin/Php/moveOut-User.php
+++ Admin/Php/moveOut-User.php
@@ -2,8 +2,17 @@
     require("../../Php-Connection.php");
 
     $userId=$_GET['userId'];
-    if(mysqli_query($conn, "UPDATE users SET isMoved = 1 , moveOutDate = CURRENT_DATE() WHERE userId = '$userId' ")){
-        header("location: ../List-Admin.php");
-    }
-    echo date('Y-m-d');
+    if(mysqli_num_rows(mysqli_query($conn, "SELECT * FROM due_user_flat WHERE userId = '$userId' AND is_paid = 0"))){
+        echo "<script type='text/javascript'>alert('This User Has Unpaid Dues');</script>";
+        echo("<script>window.location ='../List-Admin.php';</script>");
+    }else{
+        if(mysqli_num_rows(mysqli_query($conn, "SELECT * FROM expense_user_flat WHERE userId = '$userId' AND is_paid = 0"))){
+            echo "<script type='text/javascript'>alert('This User Has Unpaid Expenses');</script>";
+            echo("<script>window.location ='../List-Admin.php';</script>");
+        }else{
+            if(mysqli_query($conn, "UPDATE users SET isMoved = 1 , moveOutDate = CURRENT_DATE() WHERE userId = '$userId' ")){
+                    header("location: ../List-Admin.php");
+            }
+        }
+    }    
 ?>
